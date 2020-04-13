086b3845d9f2ee498582624d9a6e1f6c01346bf7
HugoAbanto <HugoAbanto@bitbucket.org>
2020-04-13 11:37:48 +0200

    Adding coments in all functions using java docs format, ToDo change name project and branch to Roman/ArabicConvert Tool

--- 01_CodingBat_Project/src/main/java/com/bhs/Main/main.java
+++ 01_CodingBat_Project/src/main/java/com/bhs/Main/main.java
@@ -9,38 +9,57 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Created by JAbanto on 05.10.2017.
- */
+ * Sample Tool that convert roman numbers to arabic number and inverse
+ * Current Language ist it German
+ * @TODO Current range of number are 100
+ * <pre>
+ *     convertRomanToArabic(x) or covertArabicToRoman(x)
+  * </pre>
+ * @author JefeMaestro
+ * @param x a string value that can be arabic number[0-9] or a valid Roman number [IVXLDM]
+ *
+ * */
 public class main {
 
     public static void main(String[] args) throws IOException {
 
-        String input;
+        String inputStr = "";
         boolean endProgram = false;
 
+        /**
+         * Menu Console
+         */
         do {
 
             System.out.println("Bitte Geben Sie ein RomanischeNummer(Max C) oder ein Number bis 100");
-            input = readImput();
+            inputStr = readImput();
 
-            if (input.matches("^[IVXLDM]+$")){
-                convertRomanToArabic(input);
-            }else if (input.matches("^[0-9]+$")){
-                convertArabicToRoman(input);
-            }else if (input.matches("q.*|quit|e.*|exit")){
+            /// will be check that the Imput values are RomanNumber
+            if (inputStr.matches("^[IVXLDM]+$")){
+                convertRomanToArabic(inputStr);
+            // will be check that the numbers has the correct format
+            }else if (inputStr.matches("^[0-9]+$")){
+                convertArabicToRoman(inputStr);
+            // will be check that another options haven the correct format
+            }else if (inputStr.matches("q.*|quit|e.*|exit")){
                 System.out.println("Program wird beendet...");
                 endProgram = true;
             }else{
-                System.out.println("Ungültige Eingabe '"+input+"'.");
+                System.out.println("Ungültige Eingabe '"+inputStr+"'.");
             }
 
         }while (!endProgram);
 
     }
 
-    private static void convertArabicToRoman(String imput) {
+
+    /***
+     * Convert the checked number into a roman number
+     * @param arabicNumber validated number with the correct arabic format
+     */
+    private static void convertArabicToRoman(String arabicNumber) {
         int number;
-        number =  Integer.valueOf(imput);
+        number =  Integer.valueOf(arabicNumber);
         if ((number < 1 || number < 100)){
 
             System.out.println("Romasiche Character eingegeben");
@@ -48,13 +67,22 @@ public class main {
         }
     }
 
-    private static void convertRomanToArabic(String imput) {
+    /***
+     * Convert the checked romanNumber into a arabic number
+     * @param romanNumber string with the correct roman format
+     */
+    private static void convertRomanToArabic(String romanNumber) {
         String roman;
-        roman = imput;
+        roman = romanNumber;
         System.out.println("Romasiche Character eingegeben");
         System.out.println(roman + " arabische Nummer --> "+ RomanToNumber.convertToArabicNumber(roman));
     }
 
+    /**
+     * Read the input on the console
+     * @return return  a valid string value
+     * @throws IOException handle no expected or null exceptions
+     */
     private static String readImput() throws IOException{
 
         String enter = "";
